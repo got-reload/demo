8e3904d04a0160a9c52b719c09728cb1a1cbdf7e
Larry Clapp <larry@theclapp.org>
2024-05-10 17:12:26 -0400

    Update to work with current Yaegi
    Make  a *float64 instead of just a float64.

--- example/example.go
+++ example/example.go
@@ -7,11 +7,11 @@ import (
 )
 
 var (
-	f float64
+	f = new(float64)
 )
 
 func F1() int {
-	f += 0.1
-	fmt.Printf("f: %0.3f, sin %0.3f\n", f, example2.Sin(f))
+	*f += 0.1
+	fmt.Printf("f: %0.3f, sin %0.3f\n", *f, example2.Sin(*f))
 	return 1
 }
